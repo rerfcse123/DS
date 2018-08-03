e40f5334ba13318c8feeb3a139c487e424ad9ef1
REGENT EDUCATION & RESEARCH FOUNDATION <41721646+rerfcse123@users.noreply.github.com>
2018-08-03 13:38:35 +0530

    Add files via upload

---
+++ 2. Swap elements of Java Vector example.java
@@ -0,0 +1,28 @@
+// Swap elements of Java Vector example
+
+import java.util.Vector;
+import java.util.Collections;
+ 
+public class SwapElementsOfVectorExample {
+ 
+  public static void main(String[] args) {
+   
+    
+    Vector v = new Vector();
+   
+     
+    v.add("1");
+    v.add("2");
+    v.add("3");
+    v.add("4");
+    v.add("5");
+   
+    System.out.println("Before swaping, Vector contains : " + v);
+   
+   
+    Collections.swap(v,0,4);
+   
+    System.out.println("After swaping, Vector contains : " + v);
+   
+  }
+}
